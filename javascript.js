c0e64ad28d5e0a5ca27ab36b44282451579d7891
mabarbar <mateusz.barzenc00@gmail.com>
2022-05-30 16:22:04 +0200

    update files

---
+++ javascript.js
@@ -0,0 +1,7 @@
+const hamburger = document.querySelector(".hamburger");
+
+const handleClick = () => {
+  hamburger.classList.toggle("hamburger--active");
+};
+
+hamburger.addEventListener("click", handleClick);
